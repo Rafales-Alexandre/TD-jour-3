84a80ecccd22b241c2df2a18eb70aeae09a3bf34
alexandrerafales <92779990+alexandrerafales@users.noreply.github.com>
2021-10-24 15:02:36 +0200

    Update Exercice5.py
    rajout d'une boucle pour que le programme fonctionne correctement après la 1er réponse raté

--- Exercice5.py
+++ Exercice5.py
@@ -1,17 +1,24 @@
 import random
 
 bombe = random.randint(0,100)
-tentative = input("tentez votre chance! Proposez un entier de 0 à 100 : ")
-count=0
+print(bombe)
+tentative = input("Tentez votre chance! Proposez un entier de 0 à 100 : ")
+count=1
 i = int(tentative)-int(bombe)
+print(i)
 
 while i > 10 or i < -10:
-    if count < 4:
+    while count < 5:
         count += 1
-        test = 5 - count
+        test = 6 - count
         tentative = input(f"Raté retentez votre chance! Attention il ne vous reste plus que {test} tentatives : ")
-    elif count == 4:
+        i = int(tentative)-int(bombe)
+        if i <= 10 and i >= -10:
+            break
+    if count == 5:
         print("Game Over!")
         break
+    
+        
 if i <= 10 and i >= -10:
     print(f"Bravo vous avez trouvé la bombre en {count} tentatives")
