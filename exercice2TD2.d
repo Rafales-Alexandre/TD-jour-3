57817eb04a4edb268c9328acdc8aa6f6f6b075dd
alexandrerafales <92779990+alexandrerafales@users.noreply.github.com>
2021-10-25 09:51:26 +0200

    Create exercice2TD2.d

---
+++ exercice2TD2.d
@@ -0,0 +1,7 @@
+Exercice2TD2 :
+
+Je me suis retrouvé bloqué à la fin.
+Il ne me reste plus qu’à additionner ma valeur de décalage du mot de cryptage à l’index initiale de chaque lettre du mot à crypter.
+
+Mais je n’arrive pas à faire que le programme me prenne la valeur d’indice équivalente à l’indice dans la boucle…
+Ce qui fait que dans le meilleur des cas j’ai une valeur de codage fixe.
